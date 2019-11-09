d4f9b386064960c697490b01ddf4fc61487db14d
chengzhi <chengzhi@kuaishou.com>
2019-11-09 18:44:17 +0800

    使用AndroidX

--- app/src/main/java/com/ustc/orange/zax/MainActivity.java
+++ app/src/main/java/com/ustc/orange/zax/MainActivity.java
@@ -1,6 +1,6 @@
 package com.ustc.orange.zax;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
